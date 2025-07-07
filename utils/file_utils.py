cdb5a1691d037a870b09eb269018469965c5b28f
Dmitriy Bobylev <dmitryalexeevi4@inbox.ru>
2025-07-07 22:10:14 +0300

    Добавлен README.md

--- utils/file_utils.py
+++ utils/file_utils.py
@@ -1,16 +1,6 @@
 import uuid
 from pathlib import Path
 
-ALLOW_EXTENSIONS = [".jpg", ".png", ".gif"]
-MAX_FILE_SIZE = 5 * 1024 * 1024
-
-
-def is_allowed_file(filename: Path) -> bool:
-    """Проверяем, есть ли расширение в списке разрешенных."""
-    ext = filename.suffix.lower()
-    print(ext)
-    return ext in ALLOW_EXTENSIONS
-
 
 def get_unique_name(filename: Path) -> str:
     ext = filename.suffix.lower()
